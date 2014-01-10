a6c680275bee363a2f30f976c0dce69d981a72d6
Dennise Pintado L. <dengab_19@hotmail.com>
2014-01-09 22:06:26 -0500

    Reconociendo Linea e identificando Tags

--- IdentificandoToken.hs
+++ IdentificandoToken.hs
@@ -1,38 +1,52 @@
-import Text.Parsec
-import Text.Parsec.String
+import System.IO
+import Data.Char(toUpper)
+import Data.List
+import Data.List.Split
 
-data CInstruction = Increment | Decrement
-                    deriving (Show)
+{-funcion que crea una lista con todas las palabras  a partir de un archivo-}
+getWords :: FilePath -> IO [String]
+getWords path = do contents <- readFile path
+                   return (words contents)	
+{-Funcion Main del Proyecto-}
+main = do 
+{-Funcion getWord recoge un archivo y retorna IOSTRING []-}
+	palabras <- getWords "ejemplo.xml"
+--Metodo putStrLn se encuentra dentro del System.IO y dado un dato lo muestra en pantalla
+	putStrLn "Proyecto de Lenguajes de Programacion"
+	putStrLn "Integrantes: "
+	putStrLn " - Jonathan Mendieta"
+	putStrLn " - Denisse Pintado"
+	putStrLn " - Janina Costa"
+	putStrLn "Tema:"
+	putStrLn " - Parseo XML"
+	putStrLn ""
+	limpiador "respuesta.txt" palabras
 
-					
-parseIncrement, parseDecrement:: Parser CInstruction
-
-parseGen :: Char -> CInstruction -> Parser CInstruction
-parseGen x y = char x >> return y
-
-parseIncrement = parseGen '+' Increment
-parseDecrement = parseGen '-' Decrement
-
-
-   
-parseComment :: Parser ()
-parseComment = do
-  many $ noneOf "<>"
-  return ()
-  
-parseInstruction :: Parser CInstruction
-parseInstruction = do
-  parseComment
-  i <- parseIncrement <|> parseDecrement
-  parseComment
-  return i
-  
-parseInstructions :: Parser [CInstruction]
-parseInstructions = many parseInstruction
-					
-main :: IO ()
-main = do
-  cont <- readFile "ejemplo.xml"
-  case parse parseInstructions "ejemplo.xml" cont of
-    Left e -> print e
-    Right insn -> print insn
+--funcion que recoge una linea de codigo 
+limpiador respuesta [] = return ()
+limpiador respuesta (x:xs) = do 
+	let nuevo = split (oneOf ";{[+#:%&*}]") x 
+	printMe respuesta nuevo
+	limpiador respuesta xs
+printMe respuesta [] = return ()   
+printMe respuesta (x:xs) = do	
+			if x == "" then return ()
+			else 
+				if isInfixOf "<?" x then do
+						let sentencia = "Cabecera," ++ x ++"\n"
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else if isInfixOf "<" x then do
+						let sentencia = "TagOpen," ++ x ++"\n"
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else if isInfixOf ">" x then do
+						let sentencia = "TagClose," ++ x ++"\n"
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else do
+						let sentencia = "Expresion, " ++ x ++ "\n"
+						appendFile respuesta sentencia
