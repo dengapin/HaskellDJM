a6c656315f3d41853bcf5278b7283aa8ba704dfa
Dennise Pintado L. <dengab_19@hotmail.com>
2014-01-14 21:27:38 -0500

    Terminando de leer los atributos de los tags
    Se coloco todos los atributos de la mejor manera, esperando se pueda entender la jerarquía que existe entre nodos

--- IdentificandoToken.hs
+++ IdentificandoToken.hs
@@ -26,52 +26,113 @@ main = do
 	putStrLn "Tema:"
 	putStrLn " - Parseo XML"
 	putStrLn ""
-	limpiador "respuesta.txt" palabras
+	limpiador "respuesta.txt" palabras --Guarda en un archivo aparte
 
 --funcion que recoge una linea de codigo 
 limpiador respuesta [] = return ()
 limpiador respuesta (x:xs) = do 
-	let nuevo = splitOneOf("=<>/;{[+#:%&*}]\\\"") x 
+	let nuevo = splitOneOf("<>=\"?") x 
 	printMe respuesta nuevo
 	limpiador respuesta xs
 	
 printMe respuesta [] = return ()   
 printMe respuesta (x:xs) = do	
-			if x == "" then return ()
+			if x == " " then return()
 			else 
-				if isInfixOf "id_device" x then do
-						let sentencia = "Device," ++ x ++"\n"
+				if x == "id_device"  then do
+						let sentencia = "****************************************DEVICE***************************************"++"\n" ++"Group:"++ x ++ "::: "
 						putStrLn sentencia
 						appendFile respuesta sentencia
 						printMe respuesta xs
 				else if isInfixOf "user_agent" x then do
-						let sentencia = "Device," ++ x ++"\n"
+						let sentencia = " Device:" ++ x ++":::"
 						putStrLn sentencia
 						appendFile respuesta sentencia
 						printMe respuesta xs
-				else if isInfixOf "fallback" x then do
-						let sentencia = "Device," ++ x ++"\n"
+				else if isInfixOf "fall_back" x then do
+						let sentencia = "Device: " ++ x ++ ":::"
 						putStrLn sentencia
 						appendFile respuesta sentencia
 						printMe respuesta xs
-				else if isInfixOf "id" x then do
-						let sentencia = "Group," ++ x ++"\n"
+				else if x == "id"  then do
+						let sentencia = "**------------------------------------GROUP-------------------------------**" ++"\n" ++"Group:"++ x ++ "::: "
 						putStrLn sentencia
 						appendFile respuesta sentencia
 						printMe respuesta xs
-				else if isInfixOf "name" x then do
-						let sentencia = "Capability," ++ x ++"\n"
+				else if x =="name"  then do
+						let sentencia =  "**---------------------------CAPABILITY-------------------**" ++"\n" ++"Capability:"++ x ++ "::: "
 						putStrLn sentencia
 						appendFile respuesta sentencia
 						printMe respuesta xs
-				else if isInfixOf "value" x then do
-						let sentencia = "Capability," ++ x ++"\n"
+				else if x =="value"  then do
+						let sentencia = "Capability:" ++ x ++":::"
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else if x == "group" then do
+						let sentencia = "******TagGroup" 
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else if x == "/" then do
+						let sentencia = "---CierreTagCapability---" 
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else if x == "/group" then do
+						let sentencia = "---CierreTagGroup---" 
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else if x == "/device" then do
+						let sentencia = "---CierreTagDevice---" 
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else if x == "/devices" then do
+						let sentencia = "---CierreTagDevices---" 
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else if x == "/" then do
+						let sentencia = "---CierreTag---" 
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else if x == "device"  then do
+						let sentencia = "********TagDevice" 
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else if x == "devices"  then do
+						let sentencia = "**********TagDevices"
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else if x == "capability"  then do
+						let sentencia = "****TagCapability"
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else if x == "xml"  then do
+						let sentencia = "Cabecera_del_Documento_XML"
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+			    else if x == "version"  then do
+						let sentencia = " "++x++"::::"
+						putStrLn sentencia
+						appendFile respuesta sentencia
+						printMe respuesta xs
+				else if x == "encoding"  then do
+						let sentencia = " "++x++"::::"
 						putStrLn sentencia
 						appendFile respuesta sentencia
 						printMe respuesta xs
 				else do
-						let sentencia = "Expresion, " ++ x ++ "\n"
+						let sentencia = ">>" ++ x ++ " "++"\n"
+						putStrLn sentencia
 						appendFile respuesta sentencia
-						
+						printMe respuesta xs
 
 		   			  
